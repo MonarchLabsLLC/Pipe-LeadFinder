6f0888e03b9c049c5e2df7ce1f7e8a1b3b2ccd4b
Matt Serralta <mserralta@gmail.com>
2026-07-23 18:35:20 -0400

    Revert "feat(brand): align LeadFinder family experience"
    This reverts commit 628c752097a7fa3cf695d9a409ab389007611cf9.

--- src/components/marketing/leadfinder-header.tsx
+++ src/components/marketing/leadfinder-header.tsx
@@ -41,13 +41,10 @@ export function LeadFinderHeader() {
   return (
     <header className="pl-header">
       <div className="pl-shell pl-header__inner">
-        <div className="pl-brand-lockup">
-          <Link className="pl-brand" href="/" aria-label="PipeLeads LeadFinder home">
-            <PipeLeadsMark />
-            <span>PipeLeads <b>LeadFinder</b></span>
-          </Link>
-          <a className="pl-brand-family" href="https://scale.gg/">Part of Scale.gg <ArrowRight aria-hidden="true" /></a>
-        </div>
+        <Link className="pl-brand" href="/" aria-label="PipeLeads LeadFinder home">
+          <PipeLeadsMark />
+          <span>PipeLeads <b>LeadFinder</b></span>
+        </Link>
 
         <nav className="pl-nav" aria-label="Primary navigation">
           {navItems.map((item) => <a key={item.href} href={item.href}>{item.label}</a>)}
